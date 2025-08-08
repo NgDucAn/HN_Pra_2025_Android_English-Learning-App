347b8b92bc0e454b4cd2610c4464ebee91e9b6d7
VuLong <113624484+Nsie264@users.noreply.github.com>
2025-08-08 11:27:35 +0700

    [Feature]: Setup base project (#2)

--- app/src/main/java/com/sun/englishlearning/MainActivity.kt
+++ app/src/main/java/com/sun/englishlearning/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.sun.englishlearning
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.LayoutInflater
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.sun.englishlearning.databinding.ActivityMainBinding
+import com.sun.englishlearning.utils.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun inflateBinding(inflater: LayoutInflater): ActivityMainBinding {
+        return ActivityMainBinding.inflate(inflater)
+    }
+
+    override fun initView() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun initData() {
     }
 }
